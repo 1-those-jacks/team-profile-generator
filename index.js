380c83b200e1a0144413735b21cec66427ca92f7
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2022-12-04 13:21:30 -0600

    Set Up Require, Jest Modules. Added Generate HTML Info.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("Good Luck");
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./utils/generateHTML');
+
+function writeToFile(fileName, data){
+	fs.writeFile(fileName, generateHTML(data), function(error){
+		if(error) {
+			console.log("Could not write file.");
+		} else {
+			console.log("Success, new file created.")
+		}
+	})
+}
+
+function init() {
+	return inquirer.prompt(questions)
+		.then(data => {
+			writeToFile("orgChart.html", data);
+		});
+}
+
+init();
