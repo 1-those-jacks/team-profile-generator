b18c9b6466e7144a29e057d506b59901f91b1316
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2022-12-06 17:09:16 -0600

    Got SubClass to Test Positive. Included Super to Engineer Class.

--- __test__/engineer.test.js
+++ __test__/engineer.test.js
@@ -1,3 +1,19 @@
-const { it } = require("node:test");
+const Engineer = require("../lib/engineer");
 
-it()
+describe("Engineer", () => {
+	it("Can get the GitHub account", () => {
+		const value = "1-those-jacks";
+		const instance = new Engineer("Jack", 1, "jackdgibbs1991@gmail.com", value);
+		expect(instance.github).toBe(value);
+	});
+	it("Can get role through getRole() function", () => {
+		const value = "Engineer";
+		const instance = new Engineer("Jack", 1, "jackdgibbs1991@gmail.com", "1-those-jacks");
+		expect(instance.getRole()).toBe(value);
+	});
+	it("Can get GitHub username through getGitHub() function", () => {
+		const value = "1-those-jacks";
+		const instance = new Engineer("Jack", 1, "jackdgibbs1991@gmail.com", value);
+		expect(instance.getGitHub()).toBe(value);
+	});
+})
